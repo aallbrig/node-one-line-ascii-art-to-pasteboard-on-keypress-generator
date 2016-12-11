6eb9d2ed4fb718a264820f2e43c91e74a8d20ade
Andrew Allbright <aallbrig@gmail.com>
2016-12-11 15:04:48 -0500

    PoC program drafted.  New issues to tackle: turn program into "always on" process.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const keypress = require('keypress');
+const ncp = require('copy-paste');
+const asciiArtConfig = {
+  'jkl': '₍˄·͈༝·͈˄₎◞ ̑̑'  // This renders as a cat, I swear :)
+};
+const bufferLimit = 10;
+let sequenceBuffer = [];
 
 keypress.enableMouse(process.stdout);
 keypress(process.stdin);
@@ -8,8 +14,24 @@ process.stdin.on('mousepress', (info) =>
 
 process.stdin.on('keypress', (ch, key) => {
   console.log('got "keypress"', key);
+  sequenceBuffer = [...sequenceBuffer, key];
+  if (sequenceBuffer.length > bufferLimit) {
+    const [_, newSequenceBuffer] = sequenceBuffer;
+    sequenceBuffer = newSequenceBuffer;
+  }
+  sequenceBufferStr = sequenceBuffer
+    .filter(_ => _) // Filter undefined keypresses
+    .map(_ => _.name)
+    .join('');
+  console.log('buffer', sequenceBufferStr);
+  Object.keys(asciiArtConfig).map(targetKey => {
+    if (new RegExp(targetKey,'g').test(sequenceBufferStr)) {
+      const asciiArt = asciiArtConfig[targetKey];
+      ncp.copy(asciiArt, () => console.log(`Copied ${asciiArt} into buffer`))
+      sequenceBuffer = [];
+    }
+  })
   if (key && key.ctrl && key.name == 'c') {
-    console.log('pause ', key);
     process.stdin.pause();
   }
 });
